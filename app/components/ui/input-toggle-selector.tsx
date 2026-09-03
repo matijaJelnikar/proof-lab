8dc26b95050b13dece74bab6eb13d9e2b5efa093
Matija Jelnikar <mtyajelnikar@gmail.com>
2026-09-03 21:03:37 +0200

    feat: add toggle selector

---
+++ app/components/ui/input-toggle-selector.tsx
@@ -0,0 +1,22 @@
+
+export default function InputToggleSelector<T extends string>({ options, selectedOption, onChange }: { options: Record<T, String>, selectedOption: T, onChange: (option: T) => void }) {
+    return (
+        <div className='flex flex-col gap-1'>
+
+            <div className='flex gap-2'>
+                {Object.entries(options).map(([key, value]) => (
+                    <button
+                        key={String(key)}
+                        onClick={() => onChange(key as T)}
+                        className={`px-4 py-2 cursor-pointer rounded-md border ${selectedOption === key ? 'bg-accent text-background' : 'bg-surface text-foreground'}`}
+                    >
+                        {String(key)}
+                    </button>
+                ))}
+            </div>
+            {selectedOption && (
+                <p className='text-sm text-muted'>Selected: {options[selectedOption]}</p>
+            )}
+        </div>
+    )
+}
