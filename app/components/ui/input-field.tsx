deb0e31b031fe4875208227113fbdc99248dac69
Matija Jelnikar <mtyajelnikar@gmail.com>
2026-09-03 10:53:22 +0200

    feat: add input field component

---
+++ app/components/ui/input-field.tsx
@@ -0,0 +1,47 @@
+interface InputFieldProps {
+    name: string;
+    label: string;
+    placeholder: string;
+    unit?: string;
+    step?: number;
+    value?: string | number;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+
+export default function InputField({
+    name,
+    label,
+    placeholder,
+    unit,
+    step = 1,
+    value,
+    onChange
+}: InputFieldProps) {
+    return (
+        <div className='flex flex-col gap-1'>
+            <label htmlFor={name}>{label}</label>
+            <div className='relative'>
+                <input
+                    id={name}
+                    type="number"
+                    name={name}
+                    placeholder={placeholder}
+                    step={step}
+                    value={value}
+                    onChange={onChange}
+                    required
+                    className={`min-h-11 w-full rounded-md border  pl-3 border-border bg-surface ${unit ? 'pr-9' : 'pr-3'}`}
+                />
+                {unit && (
+                    <span
+                        aria-hidden="true"
+                        className='pointer-events-none absolute inset-y-0 right-3 flex items-center text-sm text-foreground'
+                    >
+                        {unit}
+                    </span>
+                )}
+            </div>
+        </div>
+    )
+}
