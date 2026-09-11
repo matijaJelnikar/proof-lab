37a7910cbe85f4f83c2d4d9630ab5977f9866dbe
Matija Jelnikar <mtyajelnikar@gmail.com>
2026-09-11 09:45:31 +0200

    fix: remove back button from bread calculator

--- app/bread/page.tsx
+++ app/bread/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 export default function BreadPage() {
     return (
         <div className='flex flex-col w-full'>
-            <Link href={'/'} className='text-accent hover:underline flex items-start p-4'>Go back</Link>
             <div className="flex flex-col self-center max-w-3xl justify-center h-full gap-6">
                 <BreadCalculator />
             </div>
